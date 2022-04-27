ec07f7c4e96c2c324ed5ec09452e0f11ce2a8f68
kazuhito28gou <kazuhito55@gmail.com>
2022-04-27 18:29:10 +0900

    Start upload

---
+++ YoshidayaGadgetsCS/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace YoshidayaGadgetsCS
+{
+    partial class formGadgets
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.viewGadgets = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.viewGadgets)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // viewGadgets
+            // 
+            this.viewGadgets.AllowExternalDrop = true;
+            this.viewGadgets.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.viewGadgets.CreationProperties = null;
+            this.viewGadgets.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.viewGadgets.Location = new System.Drawing.Point(2, 2);
+            this.viewGadgets.Name = "viewGadgets";
+            this.viewGadgets.Size = new System.Drawing.Size(797, 448);
+            this.viewGadgets.TabIndex = 0;
+            this.viewGadgets.ZoomFactor = 1D;
+             // 
+            // formGadgets
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.viewGadgets);
+            this.Name = "formGadgets";
+            this.Text = "吉田屋事務器";
+            this.Load += new System.EventHandler(this.formGadgets_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.viewGadgets)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 viewGadgets;
+    }
+}
+
---
+++ YoshidayaGadgetsCS/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+using System.Diagnostics;
+
+namespace YoshidayaGadgetsCS
+{
+
+    public partial class formGadgets : Form
+    {
+
+        string StartURL = "file://server02/data/%EF%BE%8E%EF%BD%BD%EF%BE%84%EF%BE%83%EF%BE%9E%EF%BD%B0%EF%BE%80/desktop/index.html";
+        //string StartURL = "file:///C:/Users/user/source/repos/YoshidayaGadgetsCS/YoshidayaGadgetsCS/test.html";
+
+        public formGadgets()
+        {
+            InitializeComponent();
+
+            this.viewGadgets.Source = new System.Uri(StartURL, System.UriKind.Absolute);
+
+        }
+
+        private async Task InitializeAsync()
+        {
+            await viewGadgets.EnsureCoreWebView2Async(null); // CoreWebView2初期化待ち
+            viewGadgets.CoreWebView2.NewWindowRequested += CoreWebView2_NewWindowRequested;
+        }
+
+        private void formGadgets_Load(object sender, EventArgs e)
+        {
+            viewGadgets.NavigationStarting += WebView2_NavigationStarting;
+
+            _ = InitializeAsync();
+
+        }
+
+        private void WebView2_NavigationStarting(object sender, CoreWebView2NavigationStartingEventArgs e)
+        {
+
+            if (!e.Uri.Contains(StartURL))
+            {
+                //MessageBox.Show("NavigationStarting");
+                Process.Start(e.Uri); // デフォルトブラウザで開く
+                e.Cancel = true; // webview2内でのアクセスをキャンセル
+
+                return;
+            }
+        }
+
+        private void CoreWebView2_NewWindowRequested(object sender, CoreWebView2NewWindowRequestedEventArgs e)
+        {
+            e.Handled = true; // NewWindowのキャンセル
+
+            if (!e.Uri.Contains(StartURL))
+            {
+                //MessageBox.Show("NewWindowRequested");
+                Process.Start(e.Uri);
+                return;
+            }
+        }
+
+    }
+}
